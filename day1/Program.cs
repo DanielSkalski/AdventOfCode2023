a48c555019b807b95abc905bbd22ed0895d9d04d
Daniel Skalski <daniel.skalski@gmail.com>
2023-12-08 22:07:42 +0100

    Day 1 part 1 solution

---
+++ day1/Program.cs
@@ -0,0 +1,13 @@
+﻿var filepath = "./data2.txt";
+
+var sum = 0;
+
+foreach(var line in await File.ReadAllLinesAsync(filepath))
+{
+    var firstDigit = line.First(char.IsDigit);
+    var lastDigit =line.Last(char.IsDigit);
+
+    sum += int.Parse(new string([firstDigit, lastDigit]));
+}
+
+Console.Write(sum);
